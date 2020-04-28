05dd7b8152a22b523a1c45b4fadf42e99af50748
AnyOldName3 <krizdjali+gitlab@gmail.com>
2020-04-28 16:28:14 +0000

    Get rid of memory logger as there's free memory when things die
    The same error can happen when trying to allocate too much contiguous
    memory, e.g. if it's a huge block or the virtual address space is
    fragmented

--- CI/MemoryLogger.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Total memory: $((Get-WmiObject win32_operatingsystem -ComputerName $env:COMPUTERNAME).TotalVisibleMemorySize)"
-while ($True) {
-	Start-Sleep -Seconds 5
-	$Computer = Get-WmiObject win32_operatingsystem -ComputerName $env:COMPUTERNAME
-	Write-Host Used memory $($Computer.TotalVisibleMemorySize - $Computer.FreePhysicalMemory)
-}
