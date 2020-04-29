1fc0bb7bb80469187d1a343c0d42df553669fdd4
AnyOldName3 <krizdjali+gitlab@gmail.com>
2020-04-29 21:37:20 +0000

    make builds fail when they don't succeed

--- CI/GitLabWindows.ps1
+++ CI/GitLabWindows.ps1
@@ -5,14 +5,27 @@ param(
     [String] $CIBuildID
 )
 
+$Successful = $True
+New-Item -Path IncompleteBuild.txt
+
 $Time = (Get-Date -Format "%H:%m")
 echo $Time
 echo "started by ${$GitLabUserName}"
+
 $MemoryLogger = Start-Process powershell CI\MemoryLogger.ps1 -RedirectStandardOutput memorylog.log -PassThru
-cd $CIProjectDir
+
 sh CI/before_script.msvc.sh -c Release -p Win64 -v 2019 -k -V
+$Successful = $Successful -and $?
 cmake --build MSVC2019_64 --target ALL_BUILD --config Release
-echo "Exit status $?"
-cd MSVC2019_64\Release
+$Successful = $Successful -and $?
+
+Push-Location MSVC2019_64\Release
 7z a -tzip ..\..\OpenMW_MSVC2019_64_${$CIBuildRefName}_${$CIBuildID}.zip '*'
+Pop-Location
+
 Stop-Process $MemoryLogger
+
+Remove-Item -Path IncompleteBuild.txt
+if ($Successful) {
+    New-Item -Path SuccessfulBuild.txt
+}
