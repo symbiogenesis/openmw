b499458a5272f4aad41f32edd961a21011eb6270
AnyOldName3 <krizdjali+gitlab@gmail.com>
2020-04-29 16:29:08 +0000

    Maybe moving everything to a script makes it not break

---
+++ CI/GitLabWindows.ps1
@@ -0,0 +1,18 @@
+param(
+    [String] $GitLabUserName,
+    [String] $CIProjectDir,
+    [String] $CIBuildRefName,
+    [String] $CIBuildID
+)
+
+Time = (date -Format "%H:%m")
+echo $Time
+echo "started by ${$GitLabUserName}"
+$MemoryLogger = Start-Process powershell CI\MemoryLogger.ps1 -RedirectStandardOutput memorylog.log -PassThru
+cd $CIProjectDir
+sh CI/before_script.msvc.sh -c Release -p Win64 -v 2019 -k -V
+cmake --build MSVC2019_64 --target ALL_BUILD --config Release
+echo "Exit status $_"
+cd MSVC2019_64\Release
+7z a -tzip ..\..\OpenMW_MSVC2019_64_${$CIBuildRefName}_${$CIBuildID}.zip '*'
+Stop-Process $MemoryLogger
