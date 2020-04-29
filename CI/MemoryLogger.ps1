b6b745e5e6743a249055d728493af35dd55731df
AnyOldName3 <krizdjali+gitlab@gmail.com>
2020-04-29 22:31:18 +0000

    Fix date

--- CI/MemoryLogger.ps1
+++ CI/MemoryLogger.ps1
@@ -2,5 +2,5 @@ Write-Host "Total memory: $((Get-WmiObject win32_operatingsystem -ComputerName $
 while ($True) {
 	Start-Sleep -Seconds 1
 	$Computer = Get-WmiObject win32_operatingsystem -ComputerName $env:COMPUTERNAME
-	Write-Host "Used memory at $(date -Format '%H:%m'): $($Computer.TotalVisibleMemorySize - $Computer.FreePhysicalMemory) / $($Computer.TotalVisibleMemorySize)"
+	Write-Host "Used memory at $(Get-Date -Format '%H:%m'): $($Computer.TotalVisibleMemorySize - $Computer.FreePhysicalMemory) / $($Computer.TotalVisibleMemorySize)"
 }
