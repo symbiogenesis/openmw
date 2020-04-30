b849bbee0ed94d37a92334838bf451c38f792602
AnyOldName3 <krizdjali+gitlab@gmail.com>
2020-04-30 01:35:32 +0000

    shhhh

--- CI/MemoryLogger.ps1
+++ CI/MemoryLogger.ps1
@@ -4,5 +4,5 @@ while ($True) {
 	$Computer = Get-WmiObject win32_operatingsystem -ComputerName $env:COMPUTERNAME
 	Write-Host "Used memory at $(Get-Date -Format '%H:%m'): $($Computer.TotalVisibleMemorySize - $Computer.FreePhysicalMemory) / $($Computer.TotalVisibleMemorySize)"
 	Write-Host "Running interesting processes:"
-	Get-Process cmake,link,cl,msbuild,powershell | Sort-Object -Descending WS
+	Get-Process cmake, link, cl, msbuild, powershell -ErrorAction SilentlyContinue | Sort-Object -Descending WS | Format-List *
 }
