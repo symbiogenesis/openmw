628dc2648c8e04a0b5b74c4fc373cc7d1ce87c74
AnyOldName3 <krizdjali+gitlab@gmail.com>
2020-04-30 00:49:34 +0000

    Try and figure out what the frick is going on with memory

--- CI/MemoryLogger.ps1
+++ CI/MemoryLogger.ps1
@@ -3,4 +3,6 @@ while ($True) {
 	Start-Sleep -Seconds 1
 	$Computer = Get-WmiObject win32_operatingsystem -ComputerName $env:COMPUTERNAME
 	Write-Host "Used memory at $(Get-Date -Format '%H:%m'): $($Computer.TotalVisibleMemorySize - $Computer.FreePhysicalMemory) / $($Computer.TotalVisibleMemorySize)"
+	Write-Host "Running interesting processes:"
+	Get-Process cmake,link,cl,msbuild,powershell | Sort-Object -Descending WS
 }
